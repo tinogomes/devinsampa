d3b5584196b4539a57fbc87c92bd0c48ca5f5243
tinogomes <tinorj@gmail.com>
2009-10-26 19:35:10 -0200

    copy shared/config.* to released

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,6 +14,8 @@ set :use_sudo, false
 
 server "devinsampa", :app, :web, :db, :primary => true
 
+after "deploy:update_code", "deploy:copy_config_files"
+
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
@@ -24,4 +26,10 @@ namespace :deploy do
     desc "#{t} task is a no-op with mod_rails"
       task t, :roles => :app do ; end
     end
+
+  desc "[internal] Copy config files to current_release"
+  task :copy_config_files do
+    run "cp #{shared_path}/config/*.yml #{current_release}/config/"
+  end
 end
+
