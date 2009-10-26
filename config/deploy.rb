baf19c64c852f6e0827a73ac71cf659c818b30ae
tinogomes <tinorj@gmail.com>
2009-10-26 17:31:14 -0200

    adjustment for deploy via git with capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,19 @@
-set :application, "maratona"
+set :application, "devinsampa"
 set :user, "devinsampa"
 
 set :deploy_to, "/home/devinsampa/rails"
 
-set :repository, "."
-set :scm, :none
-set :deploy_via, :copy
-set :copy_cache,          "/tmp/my-app"
-set :copy_compression,    :gzip
-
-# set :scm, 'git'
-# set :repository, "ssh://cipriani@www.talleye.com/~/git/maratona.git"
-# set :deploy_via, :remote_cache
-# set :branch, 'master'
-# set :git_shallow_clone, 1
-# set :scm_verbose, true
+set :scm, 'git'
+set :repository, "git://github.com/tinogomes/devinsampa.git"
+set :deploy_via, :remote_cache
+set :branch, 'master'
+set :git_shallow_clone, 1
+set :scm_verbose, true
 
 set :use_sudo, false
 
 server "devinsampa", :app, :web, :db, :primary => true
+
 namespace :deploy do
   desc "Restarting mod_rails with restart.txt"
   task :restart, :roles => :app, :except => { :no_release => true } do
