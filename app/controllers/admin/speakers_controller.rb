d5db36ac90d61f7a836a79f4c966d712605e4167
tinogomes <tinorj@gmail.com>
2009-10-29 08:55:41 -0200

    change images to text on h1 and sidebar, hide unfinished pages and admin controls

--- app/controllers/admin/speakers_controller.rb
+++ app/controllers/admin/speakers_controller.rb
@@ -14,7 +14,7 @@ class Admin::SpeakersController < Admin::AdminController
 
     if @speaker.save
       flash[:notice] = 'Speaker was successfully created.'
-      redirect_to(speakers_path)
+      redirect_to(speakers_url) and return
     else
       render :action => "new"
     end
@@ -25,7 +25,7 @@ class Admin::SpeakersController < Admin::AdminController
 
     if @speaker.update_attributes(params[:speaker])
       flash[:notice] = 'Speaker was successfully updated.'
-      redirect_to(speakers_path)
+      redirect_to(speakers_url) and return
     else
       render :action => "edit"
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :user_sessions
-  map.resources :users
-
-  map.login "login", :controller => "user_sessions", :action => "new"
-  map.logout "logout", :controller => "user_sessions", :action => "destroy"
-  
-  map.namespace :admin do |admin|
-    admin.resources :speakers
-  end
-
-  map.home "/site",                  :controller => "pages", :action => "index"
-  map.speakers "/site/palestrantes", :controller => "speakers", :action => "index"
-  map.agenda "/site/programacao",    :controller => "agenda",   :action => "index"
-  map.contact "/site/contato",       :controller => "pages",   :action => "contact"
+  # map.resources :user_sessions
+  # map.resources :users
+  # 
+  # map.login "acesso", :controller => "user_sessions", :action => "new"
+  # map.logout "sair", :controller => "user_sessions", :action => "destroy"
+  # 
+  # map.namespace :admin do |admin|
+  #   admin.resources :speakers
+  # end
+  # 
+  map.home "/",                  :controller => "pages", :action => "index"
+  # map.speakers "/palestrantes", :controller => "speakers", :action => "index"
+  map.agenda "/programacao",    :controller => "agenda",   :action => "index"
+  # map.contact "/contato",       :controller => "pages",   :action => "contact"
 end
 
 # The priority is based upon order of creation: first created -> highest priority.
