7135160c9e2cb1533ff822295ab8088751500a4e
Everton Ribeiro e Celestino Gomes <devinsampa@devinsampa.com.br>
2009-11-10 16:40:59 -0200

    Plugin symbolize

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,4 +1,6 @@
 class AttendeesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :pagseguro
+  
   # GET /attendees
   # GET /attendees.xml
   def index
@@ -9,10 +11,14 @@ class AttendeesController < ApplicationController
   def payment
     @attendee = Attendee.find_by_token(params[:token])
     
-    if @attendee
+    if @attendee && @attendee.completed?
+      flash[:notice] = "Pagamento da inscrição já confirmado."
+      redirect_to(register_path)
+    elsif @attendee
       # Instanciando o objeto para geração do formulário
       @order = PagSeguro::Order.new(@attendee.token)
       @order.add :id => 1, :price => 35, :describe => "Inscrição do devinsampa"
+      session[:token] = @attendee.token
     else
       flash[:error] = "Inscrição não encontrada"
       redirect_to(register_path)
@@ -40,8 +46,35 @@ class AttendeesController < ApplicationController
       render :action => "new"
     end
   end
-  
+
   # POST /attendees/pagseguro
   def pagseguro
+    unless request.post?
+      @attendee = Attendee.find_by_token(session[:token])
+      if @attendee.completed?
+        flash[:notice] = "Pagamento da inscrição já confirmado."
+        redirect_to(register_path) and return
+      elsif @attendee.canceled?
+        flash[:error] = "Hove algum problema no processo de pagamento. Tente novamente."
+        redirect_to(payment_path(:token => @attendee.token)) and return
+      else
+        return true
+      end
+    else
+      capture_information and render :nothing => true
+    end
   end
+  
+  private
+  
+    def capture_information
+      pagseguro_notification do |notification|
+        #begin
+          attendee = Attendee.find_by_token!(notification.order_id)
+          attendee.update_payment_data(notification)
+        #rescue Exception => e
+        #  RAILS_DEFAULT_LOGGER.error("Ocorreu um erro no processo de captura da compra, error: #{e}, notification data: #{notification.inspect}")
+        #end
+      end
+    end
 end
