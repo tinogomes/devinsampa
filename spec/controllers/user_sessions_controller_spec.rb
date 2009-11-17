41439ae0407f218d8775893b2fdf6c6db4e03209
tinogomes <tinorj@gmail.com>
2009-11-17 07:15:18 -0200

    UsserSessionsController test passing

--- spec/controllers/user_sessions_controller_spec.rb
+++ spec/controllers/user_sessions_controller_spec.rb
@@ -5,7 +5,7 @@ describe UserSessionsController do
   integrate_views
   
   before(:each) do
-    @user = Factory(:user)
+    @user = Factory(:user, :username => "login", :password => "12345")
   end
   
   it "new action should render new template" do
@@ -14,21 +14,19 @@ describe UserSessionsController do
   end
   
   it "create action should render new template when model is invalid" do
-    UserSession.any_instance.stubs(:valid?).returns(false)
     post :create
     response.should render_template(:new)
   end
   
   it "create action should redirect when model is valid" do
-    UserSession.any_instance.stubs(:valid?).returns(true)
-    post :create
-    response.should redirect_to(root_url)
+    post :create, :user_session => {:username => "login", :password => "12345"}
+    response.should redirect_to(admin_url)
   end
   
   it "destroy action should destroy model and redirect to index action" do
-    user_session = UserSession.first
+    user_session = UserSession.find
     delete :destroy, :id => user_session
-    response.should redirect_to(root_url)
-    UserSession.exists?(user_session.id).should be_false
+    response.should redirect_to(home_url)
+    UserSession.find.should be_nil
   end
 end
