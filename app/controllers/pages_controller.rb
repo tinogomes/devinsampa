e449534bf6de6241f3b3edff639c9d18f53ae8e1
tinogomes <tinorj@gmail.com>
2010-07-01 18:20:18 -0300

    page cache for 2009 and contact pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 require "ostruct"
 
 class PagesController < ApplicationController
-  caches_page :index
+  caches_page :index, :photos_and_videos_2009, :contact
 
   def contact
     @contact = OpenStruct.new
