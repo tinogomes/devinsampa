0082f644e5626a8eadd28f9e6993b64296273630
tinogomes <tinorj@gmail.com>
2010-07-21 22:48:39 -0300

    change tabs to spaces

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,29 +1,29 @@
 $(document).ready(function() {
 
-	$('.box').corners('10px');
+  $('.box').corners('10px');
 
-	$("#map-zoom-out").click(function () {
-		$("#map-zoom-in").toggle();
-		$("#map-zoom-out").toggle();
-	});
+  $("#map-zoom-out").click(function () {
+    $("#map-zoom-in").toggle();
+    $("#map-zoom-out").toggle();
+  });
 
-	$("#map-zoom-in").click(function () {
-		$("#map-zoom-out").toggle();
-		$("#map-zoom-in").toggle();
-	});
+  $("#map-zoom-in").click(function () {
+    $("#map-zoom-out").toggle();
+    $("#map-zoom-in").toggle();
+  });
 
-	$(".banner textarea, .banner-quadrado textarea").focus(function() {
-		// only select if the text has not changed
-    	if(this.value == this.defaultValue) {
-     		this.select();
-    	}
-	});
+  $(".banner textarea, .banner-quadrado textarea").focus(function() {
+    // only select if the text has not changed
+      if(this.value == this.defaultValue) {
+        this.select();
+      }
+  });
 
-	$(".banner textarea, .banner-quadrado textarea").click(function() {
-		// only select if the text has not changed
-		if(this.value == this.defaultValue) {
-			this.select();
-		}
-	});
+  $(".banner textarea, .banner-quadrado textarea").click(function() {
+    // only select if the text has not changed
+    if(this.value == this.defaultValue) {
+      this.select();
+    }
+  });
 
 })
