fdf779cf09a5cdb62c37e44e07b3270bbc95751d
tinogomes <tinorj@gmail.com>
2010-07-21 23:55:23 -0300

    speakers factory

--- spec/factories/speaker.rb
+++ spec/factories/speaker.rb
@@ -4,11 +4,10 @@ require 'action_controller/test_process.rb'
 Factory.define :speaker do |speaker|
   path = "#{Rails.root}/spec/fixtures/rails.png"
   mimetype = "image/png"
-  
+
   speaker.name { Faker::Name.name }
   speaker.bio { Faker::Lorem.paragraph }
-  speaker.presentation { Faker::Lorem.sentence }
-  speaker.description { Faker::Lorem.paragraph }
   speaker.email { Faker::Internet.email }
-  speaker.uploaded_data { ActionController::TestUploadedFile.new(path, mimetype) }
+  speaker.avatar { ActionController::TestUploadedFile.new(path, mimetype) }
+  speaker.twitter { |s| Faker::Internet.user_name(s.name) }
 end
