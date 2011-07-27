88dd7a7837965f8e71a0e7d642112bb75eb3dd51
tinogomes <tinorj@gmail.com>
2011-07-26 23:31:10 -0300

    Updated contact e-mail

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,14 +1,14 @@
 class Contact < ActionMailer::Base
-  
+
   def from_site(contact, receive_a_copy = false)
     reply_to      contact.email
-    recipients    "devinsampa@gmail.com"
+    recipients    "contato@devinsampa.com.br"
     cc            contact.email if receive_a_copy
     from          "devinsampa@gmail.com"
     subject       contact.subject || "Contato via Dev in Sampa"
     body          :contact => contact
   end
-  
+
   def attendee_created(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -16,7 +16,7 @@ class Contact < ActionMailer::Base
     subject      "[devinsampa] Confirmação de inscrição"
     body         :name => attendee.name, :link => payment_url(:token => attendee.token), :free => attendee.free?
   end
-  
+
   def attendee_confirmation(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -24,7 +24,7 @@ class Contact < ActionMailer::Base
     subject      "[devinsampa] Confirmação de pagamento"
     body         :name => attendee.name, :doc => attendee.doc
   end
-  
+
   def attendee_pending(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -33,7 +33,7 @@ class Contact < ActionMailer::Base
     status       = {:pending => "Aguardando pagamento", :verifying => "Em análise"}
     body         :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
-  
+
   def attendee_problem(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -42,7 +42,7 @@ class Contact < ActionMailer::Base
     status       = {:canceled => "Cancelado", :refunded => "Devolvido"}
     body         :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
-  
+
   def attendee_unregister(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -50,7 +50,7 @@ class Contact < ActionMailer::Base
     subject      "[devinsampa] Cancelamento de inscrição"
     body         :attendee => attendee
   end
-  
+
   def attendee_will_unregister(attendee)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   attendee.email
@@ -58,7 +58,7 @@ class Contact < ActionMailer::Base
     subject      "[devinsampa] Sua inscrição será cancelada"
     body         :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
-  
+
   def alert_us(notification, request, params)
     default_url_options[:host] = "www.devinsampa.com.br"
     recipients   "tinorj@gmail.com, lfcipriani@gmail.com, nuxlli@gmail.com"
