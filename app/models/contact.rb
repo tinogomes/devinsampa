6cfb9d1aa9fecbb986fef727ec11e02a4913715a
tinogomes <tinorj@gmail.com>
2011-08-02 00:28:04 -0300

    Updated contact emails

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,69 +1,63 @@
 class Contact < ActionMailer::Base
-
   def from_site(contact, receive_a_copy = false)
-    reply_to      contact.email
+    common_settings(contact.subject || "Contato via Dev in Sampa", contact.email)
     recipients    "contato@devinsampa.com.br"
     cc            contact.email if receive_a_copy
-    from          "devinsampa@gmail.com"
-    subject       contact.subject || "Contato via Dev in Sampa"
     body          :contact => contact
   end
 
   def attendee_created(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Confirmação de inscrição"
-    body         :name => attendee.name, :link => payment_url(:token => attendee.token), :free => attendee.free?
+    common_settings "Confirmação de inscrição"
+    recipients      attendee.email
+    from            "devinsampa@gmail.com"
+    body            :name => attendee.name, :link => payment_url(:token => attendee.token), :free => attendee.free?
   end
 
   def attendee_confirmation(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Confirmação de pagamento"
-    body         :name => attendee.name, :doc => attendee.doc
+    common_settings "Confirmação de pagamento"
+    recipients      attendee.email
+    body            :name => attendee.name, :doc => attendee.doc
   end
 
   def attendee_pending(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Aguardando confirmação de pagamento pelo PagSeguro"
-    status       = {:pending => "Aguardando pagamento", :verifying => "Em análise"}
-    body         :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
+    status = {:pending => "Aguardando pagamento", :verifying => "Em análise"}
+
+    common_settings "Aguardando confirmação de pagamento pelo PagSeguro"
+    recipients      attendee.email
+    body            :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
 
   def attendee_problem(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Problemas no pagamento da inscrição"
-    status       = {:canceled => "Cancelado", :refunded => "Devolvido"}
-    body         :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
+    status = {:canceled => "Cancelado", :refunded => "Devolvido"}
+
+    common_settings "Problemas no pagamento da inscrição"
+    recipients      attendee.email
+    body            :status => status[attendee.status], :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
 
   def attendee_unregister(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Cancelamento de inscrição"
-    body         :attendee => attendee
+    common_settings "[devinsampa] Cancelamento de inscrição"
+    recipients      attendee.email
+    body            :attendee => attendee
   end
 
   def attendee_will_unregister(attendee)
-    default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   attendee.email
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Sua inscrição será cancelada"
-    body         :name => attendee.name, :link => payment_url(:token => attendee.token)
+    common_settings "[devinsampa] Sua inscrição será cancelada"
+    recipients      attendee.email
+    body            :name => attendee.name, :link => payment_url(:token => attendee.token)
   end
 
   def alert_us(notification, request, params)
+    common_settings "Algum esperto querendo fazer baixa em nome do PagSeguro"
+    recipients      "all@devinsampa.com.br"
+    body            :notification => notification.inspect, :request => request.inspect, :params => params.inspect
+  end
+
+  private
+  def common_settings(subject_, replying_to = "contato@devinsampa.com.br")
     default_url_options[:host] = "www.devinsampa.com.br"
-    recipients   "tinorj@gmail.com, lfcipriani@gmail.com, nuxlli@gmail.com"
-    from         "devinsampa@gmail.com"
-    subject      "[devinsampa] Algum esperado querendo fazer baixa em nome do PagSeguro"
-    body         :notification => notification.inspect, :request => request.inspect, :params => params.inspect
+    from          "devinsampa@gmail.com"
+    subject       "[devinsampa] #{subject_}"
+    reply_to      replying_to
   end
 end
